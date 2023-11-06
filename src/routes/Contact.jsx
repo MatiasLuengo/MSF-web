952dbf0d1d9d00f4780ef66dce096a6a62e3d2f0
Matias Luengo <matiasluengoo95@gmail.com>
2023-11-06 17:19:35 -0300

    Add work with us paragraph

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -20,6 +20,7 @@ export const Contact = ({  }) => {
             </a>
         </div>
         <iframe className=" max-xs:w-10/12 m-auto w-full sm:h-80" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13617.013068109176!2d-62.1213569!3d-31.4346938!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95cb282ffcdd8a35%3A0x16e70c1228aa22f9!2sMSF%20TECH%20SA!5e0!3m2!1sen!2sar!4v1698281007022!5m2!1sen!2sar"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <p className="w-1/2 mx-auto text-center text-2xl font-normal mt-28">Si querés formar parte de nuestro equipo dejanos tus datos. </p>
         <ContactForm/>
         </>
     )
